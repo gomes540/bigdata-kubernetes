741fca77b61964f198ea1a87c3ad1d34b70ebef0
Felipe Gomes <felipegomes540@gmailcom>
2024-11-09 00:30:06 -0300

    chore(terraform): add Kubernetes and Helm provider versions

--- infra/terraform/kubernetes/gke/felipe-dev/providers.tf
+++ infra/terraform/kubernetes/gke/felipe-dev/providers.tf
@@ -4,6 +4,16 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.10.0" # Substitua por uma versão específica, se necessário
     }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.33.0" # Substitua por uma versão específica, se necessário
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+      version = "~> 2.16.1" # Substitua por uma versão específica, se necessário
+    }
   }
 }
 
