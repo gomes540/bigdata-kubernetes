b97c81b66c3239ca7c1b3853d2e3b2afee5c214d
Felipe Gomes <felipegomes540@gmailcom>
2024-11-09 00:32:21 -0300

    chore(terraform): add outputs for GKE cluster details (name, endpoint, cert, token)

--- infra/terraform/kubernetes/gke/felipe-dev/modules/gke_cluster/outputs.tf
+++ infra/terraform/kubernetes/gke/felipe-dev/modules/gke_cluster/outputs.tf
@@ -1,11 +1,21 @@
 # Outputs no módulo gke-cluster para fornecer informações sobre o cluster criado
 
-output "cluster_endpoint" {
-  description = "Endpoint do cluster GKE"
-  value       = google_container_cluster.autopilot_cluster.endpoint
+output "k8s_name" {
+  description = "Nome do cluster GKE"
+  value       = data.google_container_cluster.gke_cluster.name
 }
 
-output "cluster_name" {
-  description = "Nome do cluster GKE"
-  value       = google_container_cluster.autopilot_cluster.name
+output "k8s_cluster_ca_certificate" {
+  description = "Certificado CA do cluster Kubernetes"
+  value       = base64decode(data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
+}
+
+output "k8s_endpoint" {
+  description = "Endpoint do cluster Kubernetes"
+  value       = data.google_container_cluster.gke_cluster.endpoint
+}
+
+output "k8s_token" {
+  description = "Token de autenticação para o cluster Kubernetes"
+  value       = data.google_client_config.default.access_token
 }
