8163d59e835b0842399f586ce0d1f2edb438165b
Felipe Gomes <felipegomes540@gmailcom>
2024-12-10 04:42:13 -0300

    feat(argocd): deploy Airbyte declaratively using ArgoCD

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -1,24 +1,24 @@
 # Criar o bucket do GCS para armazenar o estado do Terraform
-resource "google_storage_bucket" "terraform_state" {
-  name                        = var.gcs_state_bucket
-  location                    = var.region
-  uniform_bucket_level_access = true # Recomendado para segurança
+# resource "google_storage_bucket" "terraform_state" {
+#   name                        = var.gcs_state_bucket
+#   location                    = var.region
+#   uniform_bucket_level_access = true # Recomendado para segurança
 
-  versioning {
-    enabled = true # Habilita versionamento para facilitar rollback de alterações
-  }
+#   versioning {
+#     enabled = true # Habilita versionamento para facilitar rollback de alterações
+#   }
 
-  lifecycle {
-    prevent_destroy = true # Impede que o bucket seja destruído sem a remoção explícita
-  }
+#   lifecycle {
+#     prevent_destroy = true # Impede que o bucket seja destruído sem a remoção explícita
+#   }
 
-  labels = {
-    environment = "dev"
-    project     = "gke-cluster"
-    owner       = "data-team"
-  }
+#   labels = {
+#     environment = "dev"
+#     project     = "gke-cluster"
+#     owner       = "data-team"
+#   }
 
-}
+# }
 
 # Chamar o módulo para criar o cluster GKE Autopilot
 module "gke_cluster" {
@@ -53,7 +53,21 @@ provider "helm" {
 
 # Configuração do provider Kubernetes para acesso ao cluster GKE
 provider "kubernetes" {
-  host                   = module.gke_cluster.k8s_endpoint
+  host                   = "https://${module.gke_cluster.k8s_endpoint}"
   token                  = module.gke_cluster.k8s_token
   cluster_ca_certificate = module.gke_cluster.k8s_cluster_ca_certificate
 }
+
+resource "kubernetes_manifest" "airbyte_dev" {
+  depends_on = [module.argocd, module.gke_cluster]  # Garante que o ArgoCD seja instalado primeiro
+  manifest = yamldecode(file("${path.module}/../../gitops/applications/airbyte/dev/app.yaml"))
+
+  # Adicione uma propriedade para que o Terraform reprocessa o arquivo quando ele for alterado
+  lifecycle {
+    ignore_changes = [
+      # Isso vai ignorar mudanças nas propriedades do app.yaml, mas o arquivo values.yaml será monitorado
+      # manifest,
+    ]
+  }
+}
+
